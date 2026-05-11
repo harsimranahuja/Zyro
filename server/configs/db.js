29a4eaead623f110d65b6233b16e3d84c9172390
Harsimran Ahuja <pcteharsimranjotbca21b@gmail.com>
2026-05-11 11:42:11 +0530

    db change

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try{
         mongoose.connection.on('connected', ()=> console.log('Database connected'))
-        await mongoose.connect(`${process.env.MONGODB_URL}/zyro`)
+        await mongoose.connect(`mongodb+srv://harry:harry123@cluster0.macjheo.mongodb.net`)
     } catch (error){
         console.log(error.message)
     }
